fffe2ec8efb48f311d7b4819e98719ba38253d4d
Santiago <santiagosalinasoto@gmail.com>
2024-10-05 17:28:47 -0300

    idk

--- run.bat
+++ run.bat
@@ -6,4 +6,7 @@ docker rmi email_sender --force
 REM Build the new Docker image
 docker build -t email_sender ./send-email
 
+REM Build the new Docker image
+docker build -t front_end ./frontend
+
 pause
