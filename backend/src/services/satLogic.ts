935b002d8725a89b7c8f988660a1f1003f1276b8
Santiago Morón Santos <santimoron001@gmail.com>
2024-10-05 13:28:42 -0300

    Add DataAccess

--- backend/src/services/satLogic.ts
+++ backend/src/services/satLogic.ts
@@ -1,5 +1,6 @@
-
+import { insertResult } from '../dataAccess/userRequests';
 export class SatLogic {
+
 
   getSatelliteLocation = async (landsatCollection) => {
       
@@ -25,20 +26,15 @@ export class SatLogic {
       console.log('Fecha y hora de foto más reciente:', imageDate, imageTime);
 
       var geometry = latestImage.geometry(); 
-
-
       var centroid = geometry.centroid();
-
-
       var centroidCoordinates = centroid.coordinates().getInfo(); // Obtener información del centro
-
       console.log('Centro de la imagen:', centroidCoordinates);
 
       return centroidCoordinates;
   }
 
   checkSats = async () => {
-    let satellites : string[] = [ "LANDSAT/LC09/C02/T1_L2"];//["LANDSAT/LC09/C02/T1_L2", "LANDSAT/LC08/C02/T1_L2", "NASA/HLS/HLSL30/v002"];
+    let satellites : string[] = ["LANDSAT/LC09/C02/T1_L2"] // , ["LANDSAT/LC08/C02/T1_L2", "NASA/HLS/HLSL30/v002"];
 
     for (let i = 0; i < satellites.length; i++) {
       await this.checkAndSendPictures(satellites[i]);
@@ -50,7 +46,7 @@ export class SatLogic {
     console.log("Coordinates of " + satellite + ": ", coordinates);
     let usersData = this.getInterestedUsers(coordinates, satellite);
     for (let i = 0; i < usersData.length; i++) {
-      this.generateData(usersData[i]);
+      this.generateData(usersData[i], satellite);
     }
   }
     
@@ -63,7 +59,7 @@ export class SatLogic {
       return [{userMail:"santimoron001@gmail.com", coordinates:region, sat: satellite }];
     }
 
-    generateData = (userData) => {
+    generateData = (userData, satellite) => {
     var ee = require('@google/earthengine');
     var fs = require('fs');
     var https = require('https');
@@ -75,7 +71,9 @@ export class SatLogic {
         var endDate = ee.Date(Date.now());
         var startDate = endDate.advance(-20, 'day');
 
-        var collection = ee.ImageCollection("LANDSAT/LC08/C02/T1_RT")
+
+        //GET PICTURE
+        var collection = ee.ImageCollection(satellite)
             .filterDate(startDate, endDate)
             .filterBounds(userData.coordinates)
             .sort('system:time_start', false);
@@ -87,8 +85,8 @@ export class SatLogic {
         if (mosaic) {
 
           var downloadURL = mosaic.getDownloadURL({
-            name: 'Landsat_8_Uruguay_Mosaico',
-            bands: ['B4', 'B3', 'B2'], // Bandas RGB
+            name: satellite+"_mosaic",
+            bands: ['SR_B4', 'SR_B3', 'SR_B2'], // Bandas RGB
             region: userData.coordinates,
             scale: 400,
             format: 'GEO_TIFF'
@@ -97,32 +95,64 @@ export class SatLogic {
 
           console.log('Download URL for the mosaic of Landsat 8 image in Uruguay:', downloadURL);
 
-
-          const file = fs.createWriteStream(userData.userMail + ".tif");
+          let filePath = userData.userMail + ".tif";
+          const file = fs.createWriteStream(filePath);
           https.get(downloadURL, function(response) {
             response.pipe(file);
             file.on('finish', function() {
               file.close();
+              console.log(file);
               console.log("Image downloaded to " + userData.userMail + ".tif");
             });
-          }).on('error', function(err) {
-            console.error('Error downloading the image:', err.message);
-          });
+            var base64String = "";
+            fs.readFile(filePath, (err, data) => {
+              if (err) {
+                console.error("Error reading the file:", err);
+                return;
+              }
 
-        } else {
-          console.log('No images found for the specified region and date range.');
-        }
+              base64String = data.toString('base64');
+            });
+          
+              
+
+              //SPECTRAL SIGNATURE
+              var geometry = mosaic.geometry();
+              var centroid = geometry.centroid(); 
+              var spectralValues = mosaic.reduceRegion({
+                reducer: ee.Reducer.first(),
+                geometry: centroid,
+                scale: 1000
+              }).getInfo();
+          
+              console.log("Spectral values ", spectralValues); 
 
 
-          }, function(e) {
-            console.error('Error al inicializar Earth Engine: ' + e);
-          });
-        }, function(e) {
-          console.error('Error al autenticar: ' + e);
+              //METADATA IN CSV FORMAT
+              
+              mosaic.toDictionary().getInfo(function(metadata) {
+                var csvContent = convertMetadataToCSV(metadata);
+                fs.writeFileSync('landsat_image_metadata.csv', csvContent);
+                console.log('Metadata CSV saved as landsat_image_metadata.csv');
+              });
+
+              insertResult(userData.userMail, userData.sat,  base64String);
+              console.log("Result inserted correctly: ", base64String);
+            });
+          
+          }});
+
         });
 
     }
-
+    
 }
 
+function convertMetadataToCSV(metadata) {
+  var csv = 'Property,Value\n';  // Cabecera del CSV
+  for (var key in metadata) {
+    csv += key + ',' + metadata[key] + '\n';
+  }
+  return csv;
+}
 
