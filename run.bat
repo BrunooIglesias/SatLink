e670588488561fa7c259fccbb72e74a23954b415
Santiago <santiagosalinasoto@gmail.com>
2024-10-05 19:11:38 -0300

    Delete containers

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 
 REM Delete the existing Docker image if it exists
 docker rmi email_sender --force
+docker rmi front_end --force
 
 REM Build the new Docker image
 docker build -t email_sender ./send-email
