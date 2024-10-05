4305dfffbe0f49b3bd40b5db8bace95d1b4bc918
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2024-10-05 12:44:30 -0300

    feat: pin added

--- frontend/src/components/map/map.tsx
+++ frontend/src/components/map/map.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon from '../../public/leaflet/marker-icon.png';
+import markerShadow from '../../public/leaflet/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: markerIcon.src,
+    shadowUrl: markerShadow.src,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
 
 interface MapComponentProps {
     onMapClick: (lat: number, lng: number) => void;
@@ -8,6 +19,7 @@ interface MapComponentProps {
 
 const MapComponent: React.FC<MapComponentProps> = ({ onMapClick }) => {
     const mapRef = useRef<L.Map | null>(null);
+    const markerRef = useRef<L.Marker | null>(null);
 
     useEffect(() => {
         if (mapRef.current === null) {
@@ -28,6 +40,14 @@ const MapComponent: React.FC<MapComponentProps> = ({ onMapClick }) => {
                 onMapClick(lat, lng);
 
                 map.setView([lat, lng], map.getZoom());
+
+                if (markerRef.current) {
+                    map.removeLayer(markerRef.current);
+                }
+
+                const newMarker = L.marker([lat, lng], { icon: DefaultIcon }).addTo(map);
+
+                markerRef.current = newMarker;
             });
 
             map.getContainer().style.cursor = 'url(https://maps.gstatic.com/mapfiles/openhand_8_8.cur), default';
