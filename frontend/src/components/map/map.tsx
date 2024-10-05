e0fb9c5ba4ff78ddca1d52e53c66e9c246505e7c
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2024-10-05 11:55:02 -0300

    feat: map created

--- frontend/src/components/map/map.tsx
+++ frontend/src/components/map/map.tsx
@@ -4,22 +4,24 @@ import 'leaflet/dist/leaflet.css';
 
 const MapComponent: React.FC = () => {
     useEffect(() => {
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        if (typeof window !== 'undefined') {
+            const map = L.map('map', {
+                center: [40, -100],
+                zoom: 4,
+                zoomControl: true,
+                attributionControl: false,
+            });
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
-
-        L.marker([51.505, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+            L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                subdomains: 'abcd',
+                maxZoom: 19,
+            }).addTo(map);
+        }
     }, []);
 
     return (
-        <div>
-            <h1 className="text-2xl mb-4">OpenStreetMap with Leaflet</h1>
-            <div id="map" style={{ width: '100%', height: '500px' }}></div>
-        </div>
+        <div id="map" style={{ width: '100vw', height: '100vh' }}></div>
     );
 };
 
--- frontend/src/containers/map/MapContainer.tsx
+++ frontend/src/containers/map/MapContainer.tsx
@@ -2,9 +2,14 @@ import MapComponent from "@/components/map/map";
 
 const MapContainer: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-6">OpenStreetMap with Leaflet</h1>
-            <MapComponent />
+        <div className="flex flex-col h-screen">
+            <header className="bg-gray-800 text-white text-center py-4">
+                <h1 className="text-2xl font-bold">Get your Landsat img!</h1>
+            </header>
+
+            <div className="flex-1">
+                <MapComponent />
+            </div>
         </div>
     );
 };
