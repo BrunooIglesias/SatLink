9f0f0b495c8935f80960111c3d39c7afa54399e8
Santiago Morón Santos <santimoron001@gmail.com>
2024-10-06 12:25:44 -0300

    Refactor satLogic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -56,12 +56,13 @@ app.post('/preview', async (req, res) => {
   }
 
   let resultNatural  = await _satLogic.generateURL(requestPayload, "LANDSAT/LC09/C02/T1_L2", ['SR_B4', 'SR_B3', 'SR_B2']);
+  console.log(resultNatural);
   requestPayload.email = "preview2";
   let resultInfrared = await _satLogic.generateURL(requestPayload, "LANDSAT/LC09/C02/T1_L2", ['SR_B5', 'SR_B4', 'SR_B3']);
   requestPayload.email = "preview3";
   let resultVegetation = await _satLogic.generateURL(requestPayload, "LANDSAT/LC09/C02/T1_L2", ['SR_B6', 'SR_B5', 'SR_B4']);
-  let pathRow = await _satLogic.getPathRow(resultNatural);
-  res.status(200).json([resultNatural, resultInfrared, resultVegetation, pathRow]);
+  //let pathRow = await _satLogic.getPathRow(resultNatural);
+  res.status(200).json([resultNatural, resultInfrared, resultVegetation]);
 });
 
 
